c5692268e18bbba0a0b3bd5e92ba9c137bd9b916
ZharylkassynAyan <159699986+ZharylkassynAyan@users.noreply.github.com>
2026-09-22 13:04:11 +0500

    Add multiple implementations of twoSum function

--- android-sis2-kotlin/TwoSum.kt
+++ android-sis2-kotlin/TwoSum.kt
@@ -1,3 +1,20 @@
+// my solution
+
+class Solution {
+    fun twoSum(nums: IntArray, t: Int): IntArray {
+        for (i in 0 until nums.size) {
+            for (j in i + 1 until nums.size) {
+                if (nums[i] + nums[j] == t) {
+                    return intArrayOf(i, j)
+                }
+            }
+        }
+        return intArrayOf()
+    }
+}
+
+// video solution(efficient)
+
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         val positions = mutableMapOf<Int, Int>()
