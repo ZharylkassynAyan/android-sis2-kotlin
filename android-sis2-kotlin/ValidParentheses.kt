669c014d50ac7a612333cb0319c0a07d51447f37
ZharylkassynAyan <159699986+ZharylkassynAyan@users.noreply.github.com>
2026-09-22 13:25:49 +0500

    Update ValidParentheses.kt

--- android-sis2-kotlin/ValidParentheses.kt
+++ android-sis2-kotlin/ValidParentheses.kt
@@ -1,4 +1,30 @@
 class Solution {
+    fun main(s: String): Boolean {
+        val stack = mutableListOf<Char>()
+        for (i in 0 until s.length) {
+            val char = s[i]
+            if (char == '(' || char == '[' || char == '{') {
+                stack.add(char)
+            } else {
+                if (stack.isEmpty()) {
+                    return false
+                }
+                val ltIn = stack.size-1
+                val lt = stack[ltIn]
+                stack.removeAt(ltIn)
+                
+                if (char == ')' && lt != '(') return false
+                if (char == ']' && lt != '[') return false
+                if (char == '}' && lt != '{') return false
+            }
+        }
+        return stack.isEmpty()
+    }
+}
+
+// video solution
+
+class Solution {
     fun isValid(s: String): Boolean {
         val stack = mutableListOf<Char>()
 
